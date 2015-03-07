257fb563f3aa4562795a591909d0406b50d0591b
rkho <rkho@me.com>
2015-03-06 20:09:23 -0800

    Stuck about links

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,8 +3,8 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link = {};
-
   $scope.addLink = function(){
-    Links.addLink($scope.link);
+      Links.addLink($scope.link);
+      $scope.link = '';
   };
 });
